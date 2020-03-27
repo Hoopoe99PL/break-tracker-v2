a11843c098600b92db4e75d1e3f7e6fa887e822e
Lukasz Dzierzawski <lukasz-dzierzawski@o2.pl>
2020-03-27 21:00:37 +0100

    implementing fs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const fs = require('fs');
 const users = new Map();
 let queue = [];
 let allowedSlots = 2;
